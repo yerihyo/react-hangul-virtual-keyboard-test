9172550d3188d0129bd3f5b9e336286225285fbe
Moonyoung Kang <moonyoungkang@Moonyoungs-MacBook-Pro.local>
2022-07-30 08:05:41 -0700

    added react-hangul-virtual-keyboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import Keyboard from 'react-hangul-virtual-keyboard'
+import 'react-hangul-virtual-keyboard/build/css/index.css';
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
@@ -16,6 +19,8 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <Keyboard />
 
         <p className={styles.description}>
           Get started by editing{' '}
